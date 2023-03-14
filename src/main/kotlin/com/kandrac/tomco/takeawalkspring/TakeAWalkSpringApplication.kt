fd796366d9dbccd835eee919a92d43aa4a656d16
MatejKandrac <matkenkan@gmail.com>
2023-03-14 16:26:54 +0100

    Initial commit
    Signed-off-by: MatejKandrac <matkenkan@gmail.com>

---
+++ src/main/kotlin/com/kandrac/tomco/takeawalkspring/TakeAWalkSpringApplication.kt
@@ -0,0 +1,11 @@
+package com.kandrac.tomco.takeawalkspring
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TakeAWalkSpringApplication
+
+fun main(args: Array<String>) {
+    runApplication<TakeAWalkSpringApplication>(*args)
+}
