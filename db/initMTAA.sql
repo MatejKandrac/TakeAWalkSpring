c3a7b5f0130f53616955df46c72b8b8357a0fa20
MatejKandrac <matkenkan@gmail.com>
2023-04-19 23:17:18 +0200

    fixed register invalid token bug
    Signed-off-by: MatejKandrac <matkenkan@gmail.com>

--- db/initMTAA.sql
+++ db/initMTAA.sql
@@ -18,20 +18,6 @@ CREATE TABLE users
     device_token TEXT   NULL
 );
 
-insert into users
-values ('1', 'some picture', 'some email', 'some password', 'some username', 'some description', 'some device token');
-insert into users
-values ('2', 'next picture', 'next email', 'next password', 'next username', 'next description', 'next device token');
-insert into users
-values ('3', 'last picture', 'last email', 'last password', 'last username', 'last description', 'last device token');
-insert into users
-values ('4', 'last picture', 'last email', 'last password', 'Patreax', 'last description', 'last device token');
-
-insert into users
-values ('5', 'picture5', 'email5', 'password5', 'Patreax', 'description5', 'devicetoken5');
-insert into users
-values ('6', 'picture6', 'email6', 'password6', 'Patreax', 'description6', 'devicetoken6');
-
 CREATE TABLE events
 (
     id              SERIAL           NOT NULL PRIMARY KEY,
@@ -47,19 +33,6 @@ CREATE TABLE events
     CONSTRAINT event_user FOREIGN KEY (owner_id) REFERENCES users (id)
 );
 
--- insert into events
--- values ('100', '5', 'firstevent', 'false', '2020-06-22 19:10:25-07', '2020-06-22 19:10:25-07', 'firstdescription',
---         '100', '100', '0');
--- insert into events
--- values ('101', '5', 'firstevent', 'false', '2020-06-22 19:10:25-07', '2020-06-22 19:10:25-07', 'firstdescription',
---         '100', '100', '0');
--- insert into events
--- values ('102', '5', 'firstevent', 'false', '2020-06-22 19:10:25-07', '2020-06-22 19:10:25-07', 'firstdescription',
---         '100', '100', '0');
--- insert into events
--- values ('103', '5', 'firstevent', 'false', '2020-06-22 19:10:25-07', '2020-06-22 19:10:25-07', 'firstdescription',
---         '100', '100', '0');
-
 CREATE TYPE STATUS AS ENUM ('ACCEPTED', 'DECLINED', 'PENDING');
 
 
@@ -72,15 +45,6 @@ CREATE TABLE invites
     CONSTRAINT invites_event FOREIGN KEY (event_id) REFERENCES events (id),
     CONSTRAINT invites_user FOREIGN KEY (user_id) REFERENCES users (id)
 );
-
--- insert into invites
--- values ('1', '100', '5', 'Pending');
--- insert into invites
--- values ('2', '101', '5', 'Pending');
--- insert into invites
--- values ('3', '102', '5', 'Pending');
--- insert into invites
--- values ('4', '103', '6', 'Pending');
 
 CREATE TABLE locations
 (
@@ -114,13 +78,3 @@ CREATE TABLE messages
     CONSTRAINT messages_user FOREIGN KEY (user_id) REFERENCES users (id)
 );
 
--- insert into messages
--- values ('1', 100, '5', 'first ever test message', '2020-06-22 19:10:25-07');
--- insert into messages
--- values ('2', '100', '5', 'second ever test message', '2020-06-22 19:10:25-07');
--- insert into messages
--- values ('3', '100', '5', 'third ever test message', '2020-06-22 19:10:25-07');
--- insert into messages
--- values ('4', '100', '5', 'fourth ever test message', '2020-06-22 19:10:25-07');
--- insert into messages
--- values ('5', '100', '5', 'fifth ever test message', '2020-06-22 19:10:25-07');
